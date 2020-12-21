b3babfca3ffe36103310c79e26b39394ea091a6f
atinachang <atinachang@gmail.com>
2020-12-21 12:15:03 -0500

    using firebase tools, added realtime notifications on usercreate and project create, deployed to firebase hosting

--- src/components/projects/ProjectSummary.js
+++ src/components/projects/ProjectSummary.js
@@ -1,14 +1,14 @@
 import moment from 'moment';
 
 const ProjectSummary = ({project}) => {
-	const {title, authorFirstName, authorlastName, content, createdAt} = project;
+	const {title, authorFirstName, authorLastName, content, createdAt} = project;
 	// console.log(project)
 	return (
 					<div className="card z-depth-0 project-summary">
 				<div className="card-content grey-text text-darken-3">
 					<span className="card-title">{title}</span>
 					<p>{content}</p>
-					<p>Posted by {authorFirstName} {authorlastName}</p>
+					<p>Posted by {authorFirstName} {authorLastName}</p>
 					<p className="grey-text">{moment(createdAt.toDate()).calendar()}</p>
 				</div>
 			</div>
